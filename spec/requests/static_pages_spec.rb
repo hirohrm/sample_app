7b745d4431a07375fe37fd8832939d5d109a89b1
Hiro <hiroyuki.hasegawa5@gmail.com>
2014-08-24 01:38:12 -0700

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading)    { 'Sample App'}
+    let(:heading)    { 'Sample App' }
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
@@ -20,29 +20,26 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
-    let(:heading)    { 'Sample App'}
-    let(:page_title) { '' }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
     it_should_behave_like "all static pages"
-    it { should_not have_title('| Help') }
   end
 
   describe "About page" do
     before {visit about_path }
-    let(:heading)    { 'Sample App'}
-    let(:page_title) { '' }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
 
     it_should_behave_like "all static pages"
-    it { should_not have_title('| About Us') }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    let(:heading)    { 'Sample App'}
-    let(:page_title) { '' }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
     it_should_behave_like "all static pages"
-    it { should_not have_title('| Contact') }
   end
 
   it "should have the right links on the layout" do
@@ -54,9 +51,10 @@ describe "Static pages" do
     click_link "Contact"
     expect(page).to have_title(full_title('Contact'))
     click_link "Home"
+    expect(page).to have_title(full_title(''))
     click_link "Sign up now!"
     expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
-    expect(page).to have_title(full_title('sample app'))
+    expect(page).to have_title(full_title(''))
   end
 end
